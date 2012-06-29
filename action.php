c68291b43efaf7a77b2f952c85ca816d5ce31b29
Michael Hamann <michael@content-space.de>
2012-06-29 15:32:41 +0200

    Fix indexing and snippets for current DokuWiki, add indexer version

--- action.php
+++ action.php
@@ -2,34 +2,20 @@
 /**
  * SearchText Action Plugin
  * 
- * @author     Todd Augsburger <todd@rollerorgans.com>
+ * @author     Todd Augsburger, Michael Hamann <todd@rollerorgans.com>
  */
  
 if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
-require_once(DOKU_INC.'inc/parserutils.php');
 
- 
 class action_plugin_searchtext extends DokuWiki_Action_Plugin {
- 
-  /**
-   * return some info
-   */
-  function getInfo(){
-    return array(
-		 'author' => 'Todd Augsburger',
-		 'email'  => 'todd@rollerorgans.com',
-		 'date'   => '2008-02-26',
-		 'name'   => 'SearchText (action plugin)',
-		 'desc'   => "Searches pages rendered as text instead of raw wiki.\nDisplays search result snippets from text.\n(Configuration is in Admin Configuration Settings)\n\nNOTE: Requires a text renderer plugin such as the one available at http://cobs.rollerorgans.com/plugins/text.zip (see: http://wiki.splitbrain.org/plugin:text)\n\nNOTE: This plugin REQUIRES modification (to add an event) at the beginning of the idx_getPageWords() function in inc/fulltext.php :\n-    \$text     = rawWiki(\$id);\n+    \$text = trigger_event('FULLTEXT_SNIPPET_GETTEXT', \$id, 'rawWiki');\n\n",
-		 'url'    => 'http://wiki.splitbrain.org/plugin:searchtext',
-		 );
-  }
- 
   function register(&$controller) {
     $controller->register_hook('INDEXER_PAGE_ADD', 'BEFORE',  $this, '_getSearch');
-    $controller->register_hook('FULLTEXT_SNIPPET_GETTEXT', 'BEFORE',  $this, '_getSnippet');
+    $controller->register_hook('FULLTEXT_SNIPPET_CREATE', 'BEFORE',  $this, '_getSnippet');
+    $controller->register_hook('INDEXER_VERSION_GET', 'BEFORE', $this, '_indexerVersion');
+  }
+
+  function _indexerVersion(&$event, $param) {
+    $event->data['plugin_searchtext'] = '1';
   }
  
   function _getSearch(&$event, $param) {
@@ -37,8 +23,8 @@ class action_plugin_searchtext extends DokuWiki_Action_Plugin {
       global $ID;
 
       $prevID = $ID;
-      $ID = $event->data;
-      $event->data[1] .= p_cached_output(wikiFN($event->data[0]),'text');
+      $ID = $event->data['page'];
+      $event->data['body'] .= p_cached_output(wikiFN($event->data['page']),'text');
       $ID = $prevID;
       $event->preventDefault();
     }
@@ -49,10 +35,9 @@ class action_plugin_searchtext extends DokuWiki_Action_Plugin {
       global $ID;
 
       $prevID = $ID;
-      $ID = $event->data;
-      $event->result = p_cached_output(wikiFN($event->data),'text');
+      $ID = $event->data['id'];
+      $event->data['text'] = p_cached_output(wikiFN($event->data['id']),'text');
       $ID = $prevID;
-      $event->preventDefault();
     }
   }
 }
