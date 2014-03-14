1015f9d39d9e602a08fde4220cd53b5c324c8cd8
Thor Weinreich <thorweinreich@nefkom.net>
2014-03-14 01:25:19 +0100

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Thor Weinreich <thorweinreich@nefkom.net>
+ */
+$lang['snippet_in_text']       = 'Suchresultate vom durchsuchten Text anzeigen';
