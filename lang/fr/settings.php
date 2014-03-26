28fce64b7741d28a6d09a1da0dc08e8d17002ee9
Nicolas Friedli <nicolas@theologique.ch>
2014-03-26 11:50:46 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['search_in_text']        = 'Rechercher dans le texte mis en page<br />(les phrases correspondent toujours au texte brut)';
+$lang['snippet_in_text']       = 'Afficher le resultat de recherche en texte mis en page.';
