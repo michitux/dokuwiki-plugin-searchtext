78b9dc6c3250408a75e0af71d2d17d46ba6db11c
Hideaki SAWADA <chuno@live.jp>
2015-08-01 17:25:04 +0200

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['search_in_text']        = '描画処理済み文字から検索<br />（語句は常に元の wiki と一致します）';
+$lang['snippet_in_text']       = '描画処理済み文字から検索結果の抜粋を表示';
