cc63b6c059cb61297224022d6c05d511398740d3
Martin Schilliger <martin_schilliger@bluewin.ch>
2016-02-05 11:35:36 +0100

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Martin Schilliger <martin_schilliger@bluewin.ch>
+ */
+$lang['search_in_text']        = 'Suche in generiertem Text</br />(Suche wird im rohen wiki ausgeführt)';
+$lang['snippet_in_text']       = 'Zeige die Suchresultate vom generiertem Text';
