4b8456a8ca330114b1f6ef827e020b012c2dfd22
Dana <dannax3@gmx.de>
2016-02-10 15:20:04 +0100

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Dana <dannax3@gmx.de>
+ */
+$lang['search_in_text']        = 'Suchen im erstellten Text<br/>(Phrasen gleichen immer dem rohen Wiki)';
+$lang['snippet_in_text']       = 'Anzeigen von Auszügen der Suchergebnisse aus vorhandenen Texten';
