d13a36560c1effc4555794f086f8e6909e66f90f
Myeongjin <aranet100@gmail.com>
2016-06-30 09:50:12 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['search_in_text']        = '렌더된 텍스트에서 검색<br />(어구는 항상 원본 위키와 일치)';
+$lang['snippet_in_text']       = '렌더된 텍스트에서 검색 결과 스니펫 보이기';
