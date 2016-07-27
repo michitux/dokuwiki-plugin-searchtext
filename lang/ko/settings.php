41079dbc06b6315090ec2aa32d4d80dcd1148761
Myeongjin <aranet100@gmail.com>
2016-07-27 13:00:29 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -6,4 +6,4 @@
  * @author Myeongjin <aranet100@gmail.com>
  */
 $lang['search_in_text']        = '렌더된 텍스트에서 검색<br />(어구는 항상 원본 위키와 일치)';
-$lang['snippet_in_text']       = '렌더된 텍스트에서 검색 결과 스니펫 보이기';
+$lang['snippet_in_text']       = '렌더된 텍스트에서 검색 결과 스니펫 표시';
