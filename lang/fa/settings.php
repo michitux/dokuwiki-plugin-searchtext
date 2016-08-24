5baa33c8b1e1c1282970b798d5d5a0223ef70ed5
Sam01 <m.sajad079@gmail.com>
2016-08-24 16:40:38 +0200

    translation update

---
+++ lang/fa/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['search_in_text']        = 'جستجو در متن ارائه شده <br />(هم‌خوانی همیشگی عبارات با ویکی خام)';
+$lang['snippet_in_text']       = 'نمایش نتیجه‌ی قسمت جستجوشده از متن ارائه شده';
