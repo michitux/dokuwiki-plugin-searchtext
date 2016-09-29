880e0a1626c113f922f543870369a4f026ef1f5e
Arne Hanssen <arne.hanssen@getmail.no>
2016-09-29 17:55:25 +0200

    translation update

---
+++ lang/no/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Arne Hanssen <arne.hanssen@getmail.no>
+ */
+$lang['search_in_text']        = 'Søk i fremvist tekst<br />(Søkefraser vil for wikitekst alltid gi treff dersom denne også passer)';
+$lang['snippet_in_text']       = 'Vis tekstsnutter fra søket i fremvist tekst.';
