0ce7020dba7ebdda58a9a06e077d22e48f723650
qezwan <qezwan@gmail.com>
2020-10-06 00:00:50 +0200

    translation update

---
+++ lang/ckb/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['search_in_text']        = 'گەڕان لە دەقی نیشاندراو<br/>(دەستەواژەکان هەمیشە لەگەڵ ویکیی خاو دا دەگونجێن)';
+$lang['snippet_in_text']       = 'پیشاندانی پارچەی ئەنجامی گەڕان لە دەقی نیشانکراو';
